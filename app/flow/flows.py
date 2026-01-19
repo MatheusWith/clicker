928ffaba914d32440d7d6c89746e9ec1892588c5
MatheusWith <matheuswithsilva@gmail.com>
2026-01-19 11:38:44 -0300

    add flow interface, add loginFlow interface

---
+++ app/flow/flows.py
@@ -0,0 +1,14 @@
+from app.flows.action import Actions
+from abc import ABC, abstractmethod
+
+
+class Flow:
+    def __init__(self, actions: Actions):
+        self.actions = actions
+
+
+class LoginFlow(Flow,ABC):
+    @abstractmethod
+    def login(self):
+        pass
+
