32c616607f4f05f96fe4d87d6ab7b89186db36fe
MatheusWith <matheuswithsilva@gmail.com>
2026-01-20 09:55:59 -0300

    fix action imports

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from app.config.setup import configure
 from app.config.config import settings
-<<<<<<< Updated upstream
-=======
 from app.actions.actionsInt import ActionsInt
 from app.actions.actionsPyAutoGui import getActionPyAutoGUIImpl
 from app.flow.flows import LoginFlow
 from app.flow.loginFlow import getLoginFlowImpl
->>>>>>> Stashed changes
 
 
 def main():
     configure(settings=settings)
 
-<<<<<<< Updated upstream
-=======
     actions:ActionsInt = getActionPyAutoGUIImpl()
 
     login_flows: LoginFlow = getLoginFlowImpl(
@@ -25,7 +20,6 @@ def main():
 
     login_flows.login()
 
->>>>>>> Stashed changes
 
 if __name__ == "__main__":
     main()
