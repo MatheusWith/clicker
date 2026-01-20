cc130d97df56e2e5221750d481caaa4201e1e8bf
MatheusWith <matheuswithsilva@gmail.com>
2026-01-20 09:55:10 -0300

    fix action config

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from app.config.setup import configure
 from app.config.config import settings
+<<<<<<< Updated upstream
+=======
+from app.actions.actionsInt import ActionsInt
+from app.actions.actionsPyAutoGui import getActionPyAutoGUIImpl
+from app.flow.flows import LoginFlow
+from app.flow.loginFlow import getLoginFlowImpl
+>>>>>>> Stashed changes
 
 
 def main():
     configure(settings=settings)
 
+<<<<<<< Updated upstream
+=======
+    actions:ActionsInt = getActionPyAutoGUIImpl()
+
+    login_flows: LoginFlow = getLoginFlowImpl(
+        actions=actions,
+        path_to_username="app/img/login_flow/username_label.png",
+        path_to_password="app/img/login_flow/password_label.png",
+        path_to_login="app/img/login_flow/login_button.png",
+    )
+
+    login_flows.login()
+
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     main()
