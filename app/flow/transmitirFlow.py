dd714528bfc21dadf4df3f94dfa16b3f5cc78fbb
MatheusWith <matheuswithsilva@gmail.com>
2026-01-21 12:47:54 -0300

    add transmitirFlow and transmitir implement

---
+++ app/flow/transmitirFlow.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from app.flow.flows import TransmitirFlow
+from app.actions.actionsInt import ActionsInt
+from app.config.exceptions import IMGDoesntExistError
+
+class TransmitirFlowImpl(TransmitirFlow):
+    def __init__(
+        self,
+        actions:ActionsInt,
+        path_to_transmitir:str,
+    ):
+        super().__init__(actions)
+
+        path_transmitir = Path(path_to_transmitir)
+
+        if not(path_transmitir.exists() and path_transmitir.is_file()):
+            raise IMGDoesntExistError(path_to_transmitir)
+
+        self.path_to_transmitir = path_to_transmitir
+
+    def transmitir(self):
+        transmitir_x, transmitir_y = self.actions.search(
+            self.path_to_transmitir
+        )
+        self.actions.left_click(
+            transmitir_x,
+            transmitir_y
+        )
+
+def getTransmitirFlowImpl(
+    actions:ActionsInt,
+    path_to_transmitir:str,
+) -> TransmitirFlow:
+    return TransmitirFlowImpl(
+        actions=actions,
+        path_to_transmitir=path_to_transmitir,
+    )
