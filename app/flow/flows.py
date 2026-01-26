be9a2360635ae138c674b3558bd66285826d22ec
MatheusWith <matheuswithsilva@gmail.com>
2026-01-26 09:16:47 -0300

    app/img default path, add confidance on settings, change pause and confidance for float

--- app/flow/flows.py
+++ app/flow/flows.py
@@ -2,6 +2,7 @@ from app.actions.actionsInt import ActionsInt
 from abc import ABC, abstractmethod
 from pathlib import Path
 from app.config.exceptions import IMGDoesntExistError
+from app.config.config import settings
 
 class FlowInt(ABC):
     def __init__(self, actions: ActionsInt):
@@ -10,8 +11,8 @@ class FlowInt(ABC):
 class FlowAbs(FlowInt):
     def _enshure_paths_exist(self, paths_to:list[str]) -> None | Exception:
         for pst in paths_to:
-            p = Path(pst)
-            if not ( p.exists() and p.is_file()):
+            p = Path(settings.img_path + pst)
+            if not (p.exists() and p.is_file()):
                 raise IMGDoesntExistError(pst)
 
 
