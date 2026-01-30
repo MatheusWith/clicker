af1f9e675fb02beb9964fe6797b2eb53a8d33374
MatheusWith <matheuswithsilva@gmail.com>
2026-01-30 10:33:16 -0300

    cleaned main

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
-import time
 from app.config.setup import configure
 from app.config.config import settings
-from app.actions.actionsInt import ActionsInt
-from app.flow.flows import FlowInt
-from app.actions.actionsPyAutoGui import getActionPyAutoGUIImpl
-from app.flow.flows import (
-    OpenFlow,
-    LoginFlow, 
-    SellerFlow,
-    ProductFlow,
-    ClientFlow,
-    PaymentPlanFlow,
-    DocumentFlow,
-    TransmitFlow,
-    FinishFlow,
-)
-from app.flow.loginFlow import getLoginFlowImpl
-from app.flow.sellerFlow import getSellerFlowImpl
-from app.flow.productFlow import getProductFlowImpl
-from app.flow.clientFlow import getClientFlowImpl
-from app.flow.paymentPlanFlow import getPaymentPlanFlowImpl
-from app.flow.documentFlow import getDocumentFlowImpl
-from app.flow.transmitFlow import getTransmitFlowImpl
-from app.flow.finishFlow import getFinishFlowImpl
-from app.flow.openFlow import getOpenFlowImpl
 
 from app.automation.automationTransmit import getTransmitAutomationImpl
 from app.automation.automationInt import AutomationInt
